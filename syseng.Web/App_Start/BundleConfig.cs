b194be0e5e6c7d6eb60660173cad95454b01c83b
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-01-11 23:41:25 +0300

    add index page

--- syseng.Web/App_Start/BundleConfig.cs
+++ syseng.Web/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace syseng.Web
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -20,12 +19,15 @@ namespace syseng.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/css/master.css"));
+            bundles.Add(new StyleBundle("~/Content/index").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/css/master.css",
+                      "~/Content/css/index-page.css"));
         }
     }
 }
